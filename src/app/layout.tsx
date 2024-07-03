1e99da5ddae42c0f9541c2408b7ecd5ad7ff4e5f
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-07-03 17:37:22 +0100

    Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+// import { Inter } from "next/font/google";
+import { Providers } from "./providers";
+import { ProductProvider } from "@/utils/context/ProductContext";
+import { BDLMainLayout } from "@/components";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Providers>
+          <ProductProvider>
+            <BDLMainLayout>{children}</BDLMainLayout>
+          </ProductProvider>
+        </Providers>
+      </body>
     </html>
   );
 }
