0260ba25de7927ad6a16acec58d35e00ed0ecb8a
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-07-06 01:04:54 +0100

    Seo updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,25 @@ import { Providers } from "./providers";
 import { ProductProvider } from "@/utils/context/ProductContext";
 import { BDLMainLayout } from "@/components";
 
-// const inter = Inter({ subsets: ["latin"] });
-
+// Define metadata using the Metadata type
 export const metadata: Metadata = {
-  title: "Blutech Solutions LLC",
+  title: "BlueTech Solutions - IT Services & Solutions",
   description:
-    "Blutech Solutions LLC is a technology company specializing in innovative software solutions. We provide cutting-edge services in web and mobile application development, cloud computing, and digital transformation strategies. Our team is dedicated to delivering high-quality, scalable solutions that meet the unique needs of our clients. Contact us today to discover how we can help transform your ideas into reality",
+    "BlueTech Solutions LLC offers innovative IT services in web development, graphic design, network, and hardware solutions to enhance business efficiency.",
+  keywords:
+    "BlueTech Solutions, IT services, web development, graphic design, network solutions, hardware solutions, business solutions, technology provider, innovative technologies, customized solutions, business efficiency, pioneering solutions, long-term partnerships, future succes",
+  authors: [
+    {
+      name: "Blutech Solutions LLC",
+      url: "https://bluetech-department-list.vercel.app",
+    },
+  ],
+  openGraph: {
+    title: "Blutech Solutions LLC",
+    description:
+      "Blutech Solutions LLC is a technology company specializing in innovative software solutions. We provide cutting-edge services in web and mobile application development, cloud computing, and digital transformation strategies.",
+    siteName: "Blutech Solutions LLC",
+  },
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { BDLHomePageContainer } from "@/containers";
 export default function Home() {
   return (
     <>
-      <SEO prefix="Home" path="" />
       <BDLHomePageContainer />
     </>
   );
